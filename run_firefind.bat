b5583c5ae2547d6ed7db62d595f5f413b90efcdf
HeXi037 <emilglad037@gmail.com>
2025-09-15 13:01:16 +1000

    Update run_firefind.bat
    Changing bat file to not assume uvicorn is preinstalled in windows OS

--- run_firefind.bat
+++ run_firefind.bat
@@ -3,6 +3,6 @@ REM Install dependencies with python -m pip to avoid PATH issues on Windows/Wine
 python -m pip install -r backend\requirements.txt
 
 REM Launch FastAPI dev server and open default browser
-start "" bash start_dev.sh
+python -m uvicorn backend.dev_server:app --reload --host 0.0.0.0 --port 8000
 timeout /t 3 > nul
 start "" http://localhost:8000/
