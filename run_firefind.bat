f39e62b8642fe631c2e511e989df6ded6d2145ae
HeXi037 <emilglad037@gmail.com>
2025-10-20 15:53:37 +1100

    Update run_firefind.bat
    Fixed Uvicorn startup failure

--- run_firefind.bat
+++ run_firefind.bat
@@ -10,11 +10,9 @@ if not defined FIRE_FIND_API_TOKEN (
     echo [run_firefind.bat] FIRE_FIND_API_TOKEN not set; using development default 'dev-admin-token'.
 )
 
-echo [run_firefind.bat] Launching FastAPI dev server in a new window...
-start "" cmd /c "cd /d \"%~dp0\" && python -m uvicorn backend.dev_server:app --reload --host localhost --port 8000"
+echo [run_firefind.bat] Scheduling browser launch after server startup...
+start "" cmd /c "timeout /t 5 /nobreak > nul && start \"\" http://localhost:8000/"
 
-echo [run_firefind.bat] Waiting for server to start...
-timeout /t 5 /nobreak > nul
-
-echo [run_firefind.bat] Opening FireFind in default browser...
-start "" http://localhost:8000/
+echo [run_firefind.bat] Launching FastAPI dev server...
+cd /d "%~dp0"
+python -m uvicorn backend.dev_server:app --reload --host localhost --port 8000
